296434c82c0dc640072497b178fb584cdcd3170c
Hiago Prata <hcprata17@gmail.com>
2019-05-28 09:11:40 -0300

    basic routing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,17 @@
 import express from 'express'
 import session from 'express-session'
+import bodyParser from 'body-parser'
 import FileStoreGenerator from 'session-file-store'
-import uuid from 'uuid'
+import { v4 as uuid } from 'uuid'
 // import CORS from 'cors'
 
-const port = process.env.SERVER_PORT || 8000
+import api from './routes'
 
-// let sessionConfig = {
-//     genid: (req) => {
-//         console.log("inside the session config callback:")
-//         console.log(req.sessionID)
-//         return uuid()
-//     },
-//     secret: 'patati patata',
-//     resave: false,
-//     saveUninitialized: true
-//}
+const port: number = parseInt(process.env.SERVER_PORT as string) || 8000;
 
-const server = new express()
+const server = express();
 
-const FileStore = new FileStoreGenerator(session)
+const FileStore = FileStoreGenerator(session);
 
 server.use(session({
     genid: (req) => {
@@ -31,14 +23,10 @@ server.use(session({
     secret: 'patati patata',
     resave: false,
     saveUninitialized: true
-}))
-
-server.get("/", (req, res) => {
-    // console.log(req)
-    console.log("inside the / callback:")
-    console.log(req.sessionID + "\n")
-    res.send("Home\n")
-})
+}));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use('/', api);
 
 server.listen(port, () => {
     console.log("Server running on port:", port)
