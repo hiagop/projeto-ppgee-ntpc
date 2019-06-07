920b0ea96ec3dae4864f82e64b15dcf6640d608d
Hiago Prata <hcprata17@gmail.com>
2019-06-07 09:55:27 -0300

    better formating

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,32 +3,37 @@ import express from 'express';
 let api = express();
 
 api.get("/", (req, res) => {
-    res.json({ path: req.path,
-               session_id: req.sessionID })
+    res.json({ 
+                path: req.path,
+                session_id: req.sessionID })
 })
 
 api.get("/login", (req, res) => {
-    res.json({ path: req.path,
-        session_id: req.sessionID })
+    res.json({ 
+                path: req.path,
+                session_id: req.sessionID })
 })
 
 api.get("/login/callback", (req, res) => {
-    res.json({ path: req.path,
-               session_id: req.sessionID 
+    res.json({ 
+                path: req.path,
+                session_id: req.sessionID 
              })
 })
 
 api.get("/logout", (req, res) => {
-    res.json({ path: req.path,
-               session_id: req.sessionID 
+    res.json({ 
+                path: req.path,
+                session_id: req.sessionID 
              })
 })
 
 api.post("/questions", (req, res) => {
     console.log(req.body)
-    res.json({ path: req.path,
-               session_id: req.sessionID,
-               data: req.body 
+    res.json({ 
+                path: req.path,
+                session_id: req.sessionID,
+                data: req.body 
              })
 })
 
