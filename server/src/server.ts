f8ef88cbacc1195b5708844d06ec28ea4b817e23
Hiago Prata <hiagop@protonmail.com>
2019-06-18 17:09:57 -0300

    auto formatter setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express'
-import session from 'express-session'
-import bodyParser from 'body-parser'
-import FileStoreGenerator from 'session-file-store'
-import { v4 as uuid } from 'uuid'
+import express from "express";
+import session from "express-session";
+import bodyParser from "body-parser";
+import FileStoreGenerator from "session-file-store";
+import { v4 as uuid } from "uuid";
 // import CORS from 'cors'
 
-import api from './routes'
+import api from "./routes";
 
 const port: number = parseInt(process.env.SERVER_PORT as string) || 8000;
 
@@ -13,17 +13,21 @@ const server = express();
 
 const FileStore = FileStoreGenerator(session);
 
-server.use(session({
-    genid: (req) => { return uuid() },
+server.use(
+  session({
+    genid: req => {
+      return uuid();
+    },
     store: new FileStore(),
-    secret: 'patati patata',
+    secret: "patati patata",
     resave: false,
     saveUninitialized: true
-}));
+  })
+);
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
-server.use('/', api);
+server.use("/", api);
 
 server.listen(port, () => {
-    console.log("Server running on port:", port)
-})
+  console.log("Server running on port:", port);
+});
