6329315231db16b2467a2d09d566410f8bcb9b52
Hiago Prata <hiagop@protonmail.com>
2019-06-25 17:42:14 -0300

    TODO: implement login logic

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,6 +1,18 @@
+import passport from "passport";
+import { Strategy, Profile } from "passport-facebook";
 import { Request, Response } from "express";
+import fbConfig from "@configs/facebook";
+
+passport.use(
+  new Strategy(
+    fbConfig,
+    (accessToken: string, refreshToken: string, profile: Profile) => {
+      // TODO: renew accessToken and save it on db
+    }
+  )
+);
 
 export function login(req: Request, res: Response) {}
 export function logout(req: Request, res: Response) {}
 export function loginCallback(req: Request, res: Response) {}
-export function postQuestions(req: Request, res: Response) {}
+export async function postQuestions(req: Request, res: Response) {}
