53e9dcc171f4adee48a14a29bc273665397353b4
Hiago Prata <hiagop@protonmail.com>
2019-06-27 17:45:52 -0300

    removed root level package.json

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -6,48 +6,34 @@ import User from "@models/user.model";
 import axios from "axios";
 
 passport.use(
-  new Strategy(
-    fbConfig,
-    (
-      req: any,
-      accessToken: string,
-      _refreshToken: string,
-      profile: Profile,
-      done: any
-    ) => {
-      if (req.user) {
-        done();
-      }
-      User.findOne({ fbid: profile.id })
-        .then(async user => {
-          if (!user) {
-            const { clientID, clientSecret } = fbConfig;
-            const fbGraphBaseUrl = `https://graph.facebook.com`;
-            const fbGraphUrl = `/oauth/access_token?grant_type=fb_exchange_token&client_id=${clientID}&client_secret=${clientSecret}&fb_exchange_token=${accessToken}`;
+  new Strategy(fbConfig, (accessToken, _refreshToken, profile, done) => {
+    User.findOne({ fbid: profile.id })
+      .then(async user => {
+        if (!user) {
+          const { clientID, clientSecret } = fbConfig;
+          const fbGraphBaseUrl = `https://graph.facebook.com`;
+          const fbGraphUrl = `/oauth/access_token?grant_type=fb_exchange_token&client_id=${clientID}&client_secret=${clientSecret}&fb_exchange_token=${accessToken}`;
 
-            const fbGraphRes = await axios.get(fbGraphBaseUrl + fbGraphUrl);
+          const fbGraphRes = await axios.get(fbGraphBaseUrl + fbGraphUrl);
 
-            let newUser = new User({
-              fbid: profile.id,
-              email: profile.emails[0].value,
-              name: profile.displayName,
-              gender: profile.gender,
-              birthday: new Date(profile.birthday),
-              auth: {
-                access_token: fbGraphRes.data.access_token,
-                expires_on: new Date(fbGraphRes.data.expires_in)
-              }
-            });
-            done(null, newUser);
-          } else {
-            req;
-          }
-        })
-        .catch(err => {
-          done(err);
-        });
-    }
-  )
+          let newUser = new User({
+            fbid: profile.id,
+            email: profile.emails![0].value,
+            name: profile.displayName,
+            gender: profile.gender,
+            birthday: new Date(profile.birthday),
+            auth: {
+              access_token: fbGraphRes.data.access_token,
+              expires_on: new Date(fbGraphRes.data.expires_in)
+            }
+          });
+          done(null, newUser);
+        }
+      })
+      .catch(err => {
+        done(err);
+      });
+  })
 );
 
 export function login(req: Request, res: Response) {}
